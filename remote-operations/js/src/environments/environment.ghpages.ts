30be50a047c982e200db1aada7bdf0cf37eee3b9
ffa500 <hubertus.hohl@siemens.com>
2019-05-15 19:07:37 +0200

    fix(broker): try another broker url

--- remote-operations/js/src/environments/environment.ghpages.ts
+++ remote-operations/js/src/environments/environment.ghpages.ts
@@ -6,9 +6,6 @@ export const environment = {
     production: true,
 
     // Connect to a public MQTT broker over secure websocket.
-<<<<<<< HEAD
     brokerUrl: "wss://iot.eclipse.org:443/ws",
-=======
-    brokerUrl: "mqtts://test.mosquitto.org:8081",
->>>>>>> 8410e94be86c2545b83019e4e9747ec074081a93
+
 };
