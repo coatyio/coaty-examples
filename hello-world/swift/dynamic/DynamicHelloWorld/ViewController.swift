51dad8070f9f22fdcb9f8face785040f596149e2
Sandra Grujovic <sandra.grujovic@siemens.com>
2019-10-22 18:37:22 +0200

    chore: add dynamic CoatySwift hello world example

---
+++ hello-world/swift/dynamic/DynamicHelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicHelloWorld
+//
+//  Created by Sandra Grujovic on 15.10.19.
+//  Copyright © 2019 Siemens AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
