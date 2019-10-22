e4f8eaf38fdf55e866d31ebc4365ddce8b3d3a88
Johannes Rohwer <johannes.rohwer@siemens.com>
2019-10-22 18:34:38 +0200

    chore: add CoatySwift static hello world example

---
+++ hello-world/swift/static/HelloWorld/AppDelegate.swift
@@ -0,0 +1,42 @@
+//  Copyright (c) 2019 Siemens AG. Licensed under the MIT License.
+//
+//  AppDelegate.swift
+//  CoatySwift Hello World Example
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // Set starting point for application
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HelloWorldExampleViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
