bb3ecaca7479f6d01814d0db9a3c39057c916eaf
ffa500 <hubertus.hohl@siemens.com>
2020-06-12 11:03:10 +0200

    chore(browser): update webpack node polyfills to support WAMP binding in browser

--- sensor-things/js/dashboard/webpack.node-polyfills.config.js
+++ sensor-things/js/dashboard/webpack.node-polyfills.config.js
@@ -5,16 +5,21 @@ module.exports = {
     //
     // Starting with Angular CLI 6, webpack doesn't configure these shims for Node.js global variables 
     // any more by default. But some of these (process, global, Buffer, etc.) are required by 
-    // MQTT.js dependency modules when run in the browser.
+    // dependency modules of bindings (e.g. MQTT.js for MQTT binding, autobahn-js for WAMP binding)
+	// when run in the browser.
     node: {
+        // For MQTT.js
         console: false,
         global: true,
         process: true,
-        __filename: 'mock',
-        __dirname: 'mock',
+        __filename: "mock",
+        __dirname: "mock",
         Buffer: true,
-        setImmediate: true
+        setImmediate: true,
 
-        // See "Other node core libraries" for additional options.
+        // For autobahn-js
+        fs: "empty",
+
+        // See "Other node core libraries" for additional options: https://v4.webpack.js.org/configuration/node/
     }
 };
